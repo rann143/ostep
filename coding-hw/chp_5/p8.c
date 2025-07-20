6b81ee6f223aa187f3e4ee250f8cfb29b2102d8e
Daniel <dan.roder17@gmail.com>
2025-07-19 20:43:55 -0400

    finish problem 8

--- coding-hw/chp_5/p8.c
+++ coding-hw/chp_5/p8.c
@@ -8,12 +8,56 @@
 int main() {
     printf("Daddy says Hi\n");
 
+    int pipefd[2];
+
+    if (pipe(pipefd) < 0) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
     int rc = fork();
 
     if (rc < 0) {
         fprintf(stderr, "fork failed");
         exit(1);
     } else if (rc == 0) {
-        
+        close(pipefd[0]);
+
+        dup2(pipefd[1], STDOUT_FILENO);
+        printf("Hello from child1, pid:%d\n", (int)getpid());
+        exit(0);
+    } else {
+        printf("child1 completed\n");
     }
+
+    int rc2 = fork();
+
+    if (rc2 < 0) {
+        fprintf(stderr, "fork failed");
+        exit(1);
+    } else if (rc2 == 0){
+        printf("Hi, I'm child2 (pid:%d). Here's a message from child1:\n", (int)getpid());
+        close(pipefd[1]);
+
+        dup2(pipefd[0], STDIN_FILENO);
+
+        char buffer[1024];
+
+        while(fgets(buffer, sizeof(buffer), stdin) != NULL) {
+            printf("  -> %s", buffer);
+        }
+
+        close(pipefd[0]);
+        exit(0);
+    }
+
+    close(pipefd[0]);
+    close(pipefd[1]);
+
+    wait(NULL);
+    wait(NULL);
+
+    printf("both children completed");
+
+    return 0;
 }
